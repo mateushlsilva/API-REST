a76e81aae1cd6e3b56e206192b47c1067095f25c
mateushlsilva <mateushls01@gmail.com>
2022-09-23 19:37:55 -0300

    Adcionando o typescript e incrementando em forma de desenvolvimento

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express, { Request, Response, NextFunction } from 'express'
 const app = express()
 
 app.get('/status', (req:Request, res:Response, next:NextFunction) => {
-    res.status(200).send({foo: 'linux'})
+    res.status(200).send({foo: 'linux melhor sistema'})
 })
 
 
