e51dc0d324115d8d12f3402df4042966364ec443
mateushlsilva <mateushls01@gmail.com>
2022-09-23 19:23:00 -0300

    adciona dist

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("OI");
+import express, { Request, Response, NextFunction } from 'express'
+
+const app = express()
+
+app.get('/status', (req:Request, res:Response, next:NextFunction) => {
+    res.status(200).send({foo: 'linux'})
+})
+
+
+app.listen(3001,()=>{
+    console.log("Server rodando na porta 3001");
+    
+})
